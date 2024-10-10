877618ff815a3a8cece44d1b5ba2cf7b93122612
SFGrenade <25555417+SFGrenade@users.noreply.github.com>
2024-10-10 20:57:36 +0200

    initial commit

---
+++ src/LockZoteTheme.cs
@@ -0,0 +1,17 @@
+﻿using JetBrains.Annotations;
+using Modding;
+
+namespace LockZoteTheme;
+
+[UsedImplicitly]
+public class LockZoteTheme : Mod
+{
+    public LockZoteTheme() : base("Lock Zote Menu Theme")
+    {
+        Platform.Current.EncryptedSharedData.SetInt("eternalOrdealMenu", 0);
+    }
+
+    public override void Initialize()
+    {
+    }
+}
